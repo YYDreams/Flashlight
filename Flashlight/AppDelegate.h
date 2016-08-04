682efa363225247f1ae6fc44220c1114e1ac329c
YYDreams <799431668@qq.com>
2016-08-04 19:43:03 +0800

    Initial Commit

---
+++ Flashlight/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flashlight
+//
+//  Created by LOVE on 16/8/4.
+//  Copyright © 2016年 LOVE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Flashlight/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flashlight
+//
+//  Created by LOVE on 16/8/4.
+//  Copyright © 2016年 LOVE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
